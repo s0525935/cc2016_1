b70ade471aa923a274c27cad5d401be0ecc4d88d
s0525935 <s0525935@htw-berlin.de>
2016-12-12 02:55:53 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
 
     // Send the response body as "Hello Cloud"
+	var processId = null;
     var processId = process.pid;
     response.end('process id: '+processId); 
    
